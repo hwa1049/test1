b640f7b6e9ce60ec2b7cf6f6ec6aa5b06b18e053
choijh <choijh@kribb.re.kr>
2021-03-17 09:56:06 +0900

    서버 배포 테스트

--- web.js
+++ web.js
@@ -8,10 +8,13 @@ const router = require('./routes/index') (app);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
 
-var server = app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    console.log(`push test`);
+// var server = app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// })
+
+var server = app.listen(port, () => {
+    console.log(`Server running at `);
 })
